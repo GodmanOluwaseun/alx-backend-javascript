06219c69fa7c097faa3c9aac376d80bad0153f4f
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-10-10 17:34:49 +0000

    Funnction that creates object and method from objects param

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
   const employeeData = {
-    allEmployees: {...employeesList},
+    allEmployees: { ...employeesList },
     getNumberOfDepartments() {
       return Object.keys(this.allEmployees).length;
-    }
+    },
   };
 
   return employeeData;

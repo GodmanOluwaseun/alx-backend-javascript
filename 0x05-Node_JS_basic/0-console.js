ca595427a2ba4a84bc732d2f774c9e91ec1b6d8a
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-11-27 05:47:14 +0100

    prints argument to STDOUT

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,9 +3,10 @@
  * 
  * @param {string} displayMessage - The message to print. 
  */
-
+// eslint-disable-next-line no-console
 function displayMessage(message) {
     console.log(message);
 }
 
+// eslint-disable-next-line no-undef
 module.exports = displayMessage;

bec501c068bac9cc998b53316bfde04278075c64
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-04 21:24:02 +0100

    An interactive user program to greet user.

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+/**
+ * program executed via command line.
+ *
+ * @description - An interactive user program to greet user.
+ */
+
+const readline =  require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Welcome to Holberton School, what is your name?");
+
+rl.question('', (userName) => {
+  console.log(`Your name is: ${userName}`);
+  console.log("This important software is now closing");
+  rl.close();
+});

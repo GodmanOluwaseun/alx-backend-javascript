39830a6d82a4a9deb0592360658ac8f2a1af7651
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-05 20:07:22 +0100

    Read a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,19 +7,19 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const content = fs.readFileSync(path, 'utf-8')
+    const content = fs.readFileSync(path, 'utf-8');
     const lines = content.split('\n').filter(line => line.trim() !== '');
 
     if (lines.length <= 1) {
-        throw new Error('Cannot load the database');
+      throw new Error('Cannot load the database');
     }
 
     const fields = {};
-    
+
     lines.slice(1).forEach(line => {
       const [firstName, , , field] = line.split(',');
 
-      if (!fields[field]){
+      if (!fields[field]) {
         fields[field] = [];
       }
 
@@ -31,8 +31,8 @@ function countStudents(path) {
 
     Object.keys(fields).forEach(field => {
       const students = fields[field];
-      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`)
-    })
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    });
   } catch(error) {
     throw Error('Cannot load the database');
   }

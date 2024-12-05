4d7527f48555eaa2413446849b960f9dd81680c0
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-05 20:50:24 +0100

    Read database file Asynchronously.

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+/**
+ * Program that reads database file Asynchronously.
+ * @param {string} path - Path to dbase.
+ * @return {promise} Promise that resolves when OfflineAudioCompletionEvent.
+ */
+
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+  try {
+    const content = await fs.readFile(path, 'utf-8');
+    const lines = content.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const fields = {};
+
+    lines.slice(1).forEach((line) => {
+      const [firstName, , , field] = line.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstName);
+    });
+
+    const totalStudents = lines.length - 1;
+    console.log(`Number of students: ${totalStudents}`);
+
+    Object.keys(fields).forEach((field) => {
+      const students = fields[field];
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    });
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

483b4db9fb8faeb19d4b7f7db1b4a2c0b171d7c9
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-06 12:22:12 +0100

    Create small complex HTTP server.

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,11 +8,11 @@ const countStudents = require('./3-read_file_async.js');
 const dbase = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
     countStudents(dbase)
       .then((output) => {
@@ -20,7 +20,6 @@ const app = http.createServer((req, res) => {
         res.end();
       })
       .catch(() => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Cannot load the database');
       });
   } else {

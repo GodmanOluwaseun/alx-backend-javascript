73d95a34b99bee3e9ba0a62d2b7e6c490d20574c
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-06 15:35:37 +0100

    Complex http server created with express.

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,31 @@
+/**
+ * Complex http server created with express.
+ */
+
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const dbase = process.argv[2];
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).end('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n')
+  try {
+    const output = countStudents(dbase);
+    res.end(output);
+  } catch (error) {
+    res.status(500).end('Cannot load the database');
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send('404: Page Not Found');
+});
+
+app.listen(1245);
+
+module.exports = app;

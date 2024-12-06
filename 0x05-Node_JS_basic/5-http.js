b2c2403f4930f0358ec78b1ed264e9202b1abbd2
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-06 11:11:11 +0100

    Create small complex HTTP server.

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,15 +2,15 @@
  * Complex http server.
  */
 
-const countStudents = require('./3-read_file_async.js');
 const http = require('http');
+const countStudents = require('./3-read_file_async.js');
 
-const app = http.createServer((req, res) =>  {
+const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if(req.url === '/students') {
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     countStudents('database.csv')
       .then((output) => {
@@ -22,7 +22,7 @@ const app = http.createServer((req, res) =>  {
         res.end();
       });
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' } );
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404: Page Not Found');
   }
 });

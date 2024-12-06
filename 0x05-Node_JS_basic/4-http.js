953b1b8ed6ca77319915a422aefb47e64516a318
godmanoluwaseun <oluwaseuntopeajayi@gmail.com>
2024-12-06 04:07:13 +0100

    Create small HTTP server.

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,14 @@
 /**
  * A simple http server.
- * 
+ *
  */
 
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, {'content-type': 'text/plain'});
+  res.writeHead(200, { 'content-type': 'text/plain' });
   res.end('Hello Holberton School!');
-})
+});
 
 app.listen(1245);
 
